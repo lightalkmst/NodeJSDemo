864bd19a62a0b51b2b19c03ec5302d60e451e8ee
lightalkmst <rook_master2000@yahoo.com>
2016-07-25 23:02:29 -0400

    Cleanup

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -14,27 +14,29 @@ $.ajax ({
 
 cfg.prod && ng.core.enableProdMode ()
 
-var create_component = m1 => m2 => {
+var create_component = x => m1 => m2 => {
   var html = ''
   m1.template || $.ajax ({
-      url: '/' + m1.selector + '.html',
+      url: '/' + x + '.html',
       success: data => html = data,
       async: false
   })
-  return ng.core.Component (M.extend (m1) ({template: m1.template || html}))
-    .Class (M.extend (m2) ({
-      constructor: m2.constructor != {}.constructor ? m2.constructor : [function () {}]
+  return app[x] =
+    ng.core.Component (M.extend (m1) ({
+      selector: x,
+      template: m1.template || html,
+    }))
+      .Class (M.extend (m2) ({
+        constructor: m2.constructor != {}.constructor ? m2.constructor : [function () {}],
     }))
 }
 
 ;(app => {
-  app.my_app =
-    ng.router.Routes (L.map (h => ({path: '/' + h, component: app[h], name: h})) (M.keys (app)))
-      (create_component ({
-        selector: 'router',
-        directives: [ng.router.ROUTER_DIRECTIVES],
-        providers: [ng.router.ROUTER_PROVIDERS],
-      }) ({}))
+  ng.router.Routes (L.map (h => ({path: '/' + h, component: app[h], name: h})) (M.keys (app)))
+    (create_component ('app') ({
+      directives: [ng.router.ROUTER_DIRECTIVES],
+      providers: [ng.router.ROUTER_PROVIDERS],
+    }) ({}))
 
-  ng.platformBrowserDynamic.bootstrap (app.my_app, [ng.router.ROUTER_PROVIDERS])
+  ng.platformBrowserDynamic.bootstrap (app.app, [ng.router.ROUTER_PROVIDERS])
 }) (window.app || (window.app = {}))
